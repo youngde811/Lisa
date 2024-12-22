64ee56c14fe7b5a42e7cc5860a308dfbc633676a
David Young <sporty-iron883@protonmail.com>
2024-12-22 15:23:03 -0500

    Keep this file as the record of a bug report by Gaston Pepe

--- contrib/heater.lisp
+++ contrib/heater.lisp
@@ -1,26 +1,7 @@
-;; This file is part of Lisa, the Lisp-based Intelligent Software Agents platform.
 
-;; MIT License
-
-;; Copyright (c) 2000 David Young
-
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-
-;; The above copyright notice and this permission notice shall be included in all
-;; copies or substantial portions of the Software.
-
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
+;;; This sample code was contributed by Gaston Pepe to isolate a bug he found with Lisa's
+;;; TEST conditional element. The TEST CE must have been broken at least ten years ago via
+;;; an unapproved commit, while Lisa was still hosted on SourceForge.
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (when (not (find-package "LISA-HEATER"))
