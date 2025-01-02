caa00cadb4188a7a0839476013c3e6c9ec91dd1e
David Young <david.young@liveoak.bank>
2025-01-02 12:38:16 -0500

    Test file for OR CE

---
+++ examples/or.lisp
@@ -0,0 +1,58 @@
+;; This file is part of Lisa, the Lisp-based Intelligent Software Agents platform.
+
+;; MIT License
+
+;; Copyright (c) 2000 David Young
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+(in-package :lisa-user)
+
+(clear)
+
+(deftemplate hobbit ()
+  (slot name))
+
+(deftemplate frodo ()
+  (slot name)
+  (slot has-ring)
+  (slot age))
+
+(deftemplate bilbo ()
+  (slot name)
+  (slot relative)
+  (slot age))
+
+(deftemplate gandalf ()
+  (slot name)
+  (slot age))
+
+(defrule or-1 ()
+  (frodo)
+  (or (hobbit (name "bilbo"))
+      (hobbit (name "pippin")))
+  =>
+  (format t "Firing rule or-1"))
+
+(defun run ()
+  (reset)
+  (assert (frodo (name "frodo") (has-ring t) (age 55)))
+  (assert (hobbit (name "bilbo")))
+  (run))
+
