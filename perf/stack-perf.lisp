2febf1de3a0aa9b616236cf4b86113a249eed92c
David Young <david.young@liveoak.bank>
2025-02-06 13:19:49 -0500

    Initial token performance code

--- perf/stack-perf.lisp
+++ perf/stack-perf.lisp
@@ -24,25 +24,86 @@
 
 (in-package :cl-user)
 
-(defun make-stack ()
-  (sycamore:tree-set #'equal))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package "STACK-PERF")
+    (defpackage "STACK-PERF"
+      (:use "COMMON-LISP"))))
 
-(defun tree-set-first (set)
-  (sycamore:fold-tree-set
-   #'(lambda (accumulated-value key value)
-       (declare (ignore key))
-       (if (null accumulated-value)
-           value
-         accumulated-value))
-   nil set))
+(in-package #:stack-perf)
 
-(defun push-stack (item stack)
-  (sycamore:tree-set-insert stack item))
+(defclass token ()
+  ((facts :initform nil
+          :initarg :stack
+          :accessor token-facts)
+   (hash-code :initform nil
+              :accessor token-hash-code)
+   (fact-count :initform 0
+               :type (unsigned-byte 64)
+               :accessor token-fact-count)))
 
-(defun pop-stack (stack)
-  (let ((top (tree-set-first stack)))
-    (sycamore:tree-set-remove stack top)
-    top))
+(defclass fact ()
+  ((name :initform nil
+         :initarg :name
+         :reader fact-name)))
 
-(defun peek-stack (stack)
-  (tree-set-first stack))
+(defun token-top-fact-default (token)
+  (with-slots ((fact-vector facts)
+               (fact-count fact-count)) token
+    (declare (type fixnum lisa::fact-count) (type (vector t) lisa::facts))
+    (aref fact-vector (1- fact-count))))
+
+(defun token-push-fact-default (token fact)
+  (declare (optimize (speed 3) (safety 1) (debug 0)))
+  (with-slots ((fact-vector facts)
+               (fact-count fact-count)
+               (hash-code hash-code)) token
+    (declare (type fixnum fact-count) (type (vector t) fact-vector))
+    (vector-push-extend fact fact-vector)
+    (push fact hash-code)
+    (incf fact-count))
+  token)
+
+(defun token-pop-fact-default (token)
+  (declare (type token token))
+  (declare (optimize (speed 3) (safety 1) (debug 0)))
+  (with-slots ((fact-vector facts)
+               (hash-code hash-code)
+               (fact-count fact-count)) token
+    (declare (type fixnum fact-count) (type (vector t) fact-vector))
+    (unless (zerop (fill-pointer fact-vector))
+      (pop hash-code)
+      (decf fact-count)
+      (aref fact-vector (decf (fill-pointer fact-vector))))))
+
+(defun token-push-fact-gp (token fact)
+  (declare (optimize (speed 3) (safety 1) (debug 0)))
+  (with-slots ((fact-stack facts)
+               (hash-code hash-code)
+               (fact-count fact-count)) token
+    (grouping-stack:stack-push fact-stack fact)
+    (push fact hash-code)
+    (incf fact-count))
+  token)
+
+(defun token-pop-fact-gp (token)
+  (declare (type token token))
+  (declare (optimize (speed 3) (safety 1) (debug 0)))
+  (with-slots ((fact-stack facts)
+               (hash-code hash-code)
+               (fact-count fact-count)) token
+    (unless (zerop (grouping-stack:stack-size fact-stack))
+      (pop hash-code)
+      (decf fact-count)
+      (grouping-stack:stack-pop fact-stack))))
+
+(defun test-default-impl (&key (ntimes 100))
+  (let ((token (make-instance 'token :initarg (make-array 64 :initial-element nil :adjustable t :fill-pointer 0))))
+    (dotimes (i ntimes)
+      (token-push-fact-default (token (make-instance 'fact :name (format nil "frodo-~D" i)))))))
+
+(defun test-gp-impl (&key (ntimes 100))
+  (let ((token (make-instance 'token
+                              :initarg (grouping-stack:make-grouping-stack
+                                        (make-instance 'grouping-stack:sink-balancer)))))
+    (dotimes (i ntimes)
+      (token-push-fact-gp (token (make-instance 'fact :name (format nil "frodo-~D" i)))))))
