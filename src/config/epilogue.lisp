f3227ed52d0bed7a86e198e63d4969bc5370f619
David Young <david.young@liveoak.bank>
2025-02-13 13:34:05 -0500

    Release 3.9.1: just documentation

--- src/config/epilogue.lisp
+++ src/config/epilogue.lisp
@@ -54,6 +54,6 @@
                  (declare (ignore subchar arg))
                  (list 'identity (read strm t nil t)))))
   (pushnew :lisa *features*)
-  (pushnew :lisa3.9.0 *features*))
+  (pushnew :lisa3.9.1 *features*))
 
 
